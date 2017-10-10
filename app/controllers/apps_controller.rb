25c0922dcecd1d5435cb1ffdba081477e00841ee
Ivona <ivona1mircheva@gmail.com>
2017-10-10 23:10:01 +0300

    first commit

---
+++ app/controllers/apps_controller.rb
@@ -0,0 +1,6 @@
+class AppsController < ActionController::Base
+  def sums
+    # put any code here that you need
+    # (although for a static view you probably won't have any)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+
+Rails.application.routes.draw do
+
+  get 'welcome/index'
+  root 'welcome#index'
+
+  match '/sums', :to => 'application#sums', :via => [:post]
+
+  match '/filters', :to => 'application#filters', :via => [:post]
+
+  match '/calc', :to => 'application#calc', :via => [:post]
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
